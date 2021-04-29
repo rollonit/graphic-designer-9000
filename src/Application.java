7c170c5d1efc13135e4a2994ebfa4ab8ef36f071
rollonit <46538361+rollonit@users.noreply.github.com>
2021-04-29 19:10:44 +0200

    Initial commit of the main class

---
+++ src/Application.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Application extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("Application");
+	}
+
+	public void settings() {
+		size(700, 500);
+	}
+
+	public void setup() {
+
+	}
+
+	public void draw() {
+
+	}
+}
