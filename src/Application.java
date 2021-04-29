e0574c558ccd7a20b7003e88dbac20f5583c7d65
rollonit <46538361+rollonit@users.noreply.github.com>
2021-04-29 20:37:41 +0200

    Added basics of Application and Canvas classes

--- src/Application.java
+++ src/Application.java
@@ -2,20 +2,23 @@ import processing.core.PApplet;
 
 public class Application extends PApplet {
 
+	Canvas mainApp;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PApplet.main("Application");
 	}
 
 	public void settings() {
-		size(700, 500);
+		size(1280, 720);
 	}
 
 	public void setup() {
-
+		mainApp = new Canvas(this);
+		mainApp.init();
 	}
 
 	public void draw() {
-
+		mainApp.draw();
 	}
 }
