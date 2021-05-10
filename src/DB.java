2cd8e23825b6b4bc07a7cd061b837b836788f9a0
klemensfliri <klemens.fliri@hirschmann-automotive.com>
2021-05-10 18:05:09 +0200

    updated the DB class

--- src/DB.java
+++ src/DB.java
@@ -1,4 +1,54 @@
+import de.bezier.data.sql.*;
+import processing.core.PApplet;
+
 
 public class DB {
 
-}
+	SQLite db;
+	
+	
+	
+	public DB() {
+		db = new SQLite(null, "data/db.sql");
+		try {
+			if (db.connect()) {
+				db.query("SELECT * FROM shape" );
+				
+				while (db.next())
+				{
+					PApplet.print (db.getInt("#i")+ ", ");
+					PApplet.print (db.getInt("#s")+ ", ");
+					PApplet.print (db.getInt("x")+ ", ");
+					PApplet.print (db.getInt("y")+ ", ");
+					PApplet.print (db.getInt("h")+ ", ");
+					PApplet.print (db.getInt("w")+ ", ");
+					PApplet.print (db.getInt("stroke")+ ", ");
+					PApplet.print (db.getInt("color")+ ", ");
+					
+					PApplet.println();
+				}
+				
+				//db.query("UPDATE shape SET )
+				
+				try {
+			        db.query( "INSERT INTO contacts VALUES(\"Oak Trebb\", \"Test 1 \", 4, 123)");
+			      } catch (Exception e) {
+			        //Your only way to see whether an UPDATE or INSERT statement worked 
+			        //is when no exception occurred 
+			        e.printStackTrace();
+			      }
+			} 
+		}
+			catch (Exception e) {
+				  e.printStackTrace();
+					}
+						
+			
+		} 
+	}
+	
+	
+	
+	
+
+
