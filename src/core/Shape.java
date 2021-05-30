ac4706cf2133b53700aeddb82d26779bc7b7ff20
rollonit <46538361+rollonit@users.noreply.github.com>
2021-05-30 06:44:12 +0200

    Added documentation to the Shape class.

--- src/core/Shape.java
+++ src/core/Shape.java
@@ -35,19 +35,59 @@ public abstract class Shape {
 	private boolean isDragging;
 	protected ShapeType type;
 
+	/**
+	 * Cretaes a new shape and associates it with the main pApplet.
+	 * 
+	 * @param pApplet The handle of the main pApplet.
+	 */
 	public Shape(PApplet pApplet) {
 		this.pApplet = pApplet;
 		this.name = "";
 	}
 
+	/**
+	 * Creates a new shape with the given characteristics.
+	 * 
+	 * @param x      The X-coordinate of the new shape to create
+	 * @param y      The Y-coordinate of the new shape to create
+	 * @param w      The width of the new shape to create
+	 * @param h      The height of the new shape to create
+	 * @param stroke The stroke of the new shape to create (currently unused)
+	 * @param color  The color of the new shape to create
+	 */
 	public abstract void create(int x, int y, int w, int h, int stroke, int color);
 
+	/**
+	 * Updates the internal PShape if any of the attribues have changed.
+	 */
 	public abstract void update();
 
+	/**
+	 * Checks if the mouse currently is within the shape.
+	 * 
+	 * @return True if the mouse is in the shape
+	 */
 	public abstract boolean isInShape();
 
+	/**
+	 * Hilights all the vertices of the shape with little red circles, used so that
+	 * the user can see if the shape is selected.
+	 */
 	public abstract void highlightVertices();
 
+	/**
+	 * Translates possible wonky X, Y, width and height parameters (for example, if
+	 * the shape is created in a bottom left to top right direction, creating
+	 * negative width and height) to proper attributes with a positive width and
+	 * height.
+	 * 
+	 * @param x X-coordinate of the shape
+	 * @param y Y-coordinate of the shape
+	 * @param w Width of the shape
+	 * @param h Height of the shape
+	 * @return An int array with the new corrected values in the format of [x, y, w,
+	 *         h]
+	 */
 	protected static int[] translate(int x, int y, int w, int h) {
 		if (h >= 0 && w >= 0) {
 			int[] out = { x, y, w, h };
@@ -64,6 +104,12 @@ public abstract class Shape {
 		}
 	}
 
+	/**
+	 * Assigns the coordinates of the shape from an int array. This format of int
+	 * array is an output of the translate function.
+	 * 
+	 * @param coords An int array in the format [x, y, w, h]
+	 */
 	protected void assignCoords(int[] coords) {
 		this.x = coords[0];
 		this.y = coords[1];
@@ -72,6 +118,9 @@ public abstract class Shape {
 		System.out.println(coords[0] + ", " + coords[1] + ", " + coords[2] + ", " + coords[3] + ".");
 	}
 
+	/**
+	 * Standard draw call for the shapes. To be called every frame.
+	 */
 	public void draw() {
 		this.pApplet.shape(this.shape);
 
@@ -79,6 +128,16 @@ public abstract class Shape {
 			this.highlightVertices();
 	}
 
+	/**
+	 * A static method that draws a temporary shape with the given characteristics for one frame. Used to temporarily and dynamically display shapes.
+	 * @param p The pApplet object handle
+	 * @param x	The X-coordinate of the shape to draw
+	 * @param y	The Y-coordinate of the shape to draw
+	 * @param w	The width of the shape to draw
+	 * @param h	The height of the shape to draw
+	 * @param color The color of the shape to draw
+	 * @param type The type of the shape to draw. Currently supported are SQUARE, TRIANGLE and ELLIPSE.
+	 */
 	public static void draw(PApplet p, int x, int y, int w, int h, int color, ShapeType type) {
 		p.fill(color);
 		switch (type) {
@@ -97,34 +156,55 @@ public abstract class Shape {
 		}
 	}
 
+	/**
+	 * Moves this shape relatively by a certain amount. Use moveTo() for absolute position.
+	 * @param x The amount to change the X-coordinate by.
+	 * @param y The amount to change the Y-coordinate by.
+	 */
 	public void moveBy(int x, int y) {
 		this.setX(this.getX() + x);
 		this.setY(this.getY() + y);
 		this.update();
 	}
 
+	/**
+	 * Moves the shape to an absolute point. Use moveBy() for relative movements.
+	 * @param x The new X-coordinate.
+	 * @param y The new Y-coordinate.
+	 */
 	public void moveTo(int x, int y) {
 		this.setX(x);
 		this.setY(y);
 		this.update();
 	}
 
+	/**
+	 * Selects this current shape. This highlights the current shape.
+	 */
 	public void select() {
 		this.isSelected = true;
 		pApplet.cursor(PApplet.HAND);
 		System.out.println("Shape Selected!");
 	}
 
+	/**
+	 * Deslects this shape and stops highlighting it.
+	 */
 	public void deselect() {
 		this.isSelected = false;
 		pApplet.cursor(PApplet.ARROW);
 		System.out.println("Shape Deselected");
 	}
 
+	/**
+	 * @return True if the current shape is selected.
+	 */
 	public boolean isSelected() {
 		return this.isSelected;
 	}
 
+	//Getters and setters
+	
 	public String getName() {
 		return this.name;
 	}
